b424000e3e87e82ed0265d5770f7d529af370155
KPDawson24 <kdawson@conncoll.edu>
2015-07-23 23:05:03 -0400

    Complete Prog. Assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,49 @@
-## Put comments here that give an overall description of what your
-## functions do
+##The following two functions return the inverse of a matrix.
+##Matrix [A] is the inverse of Matrix [B] if and only if
+##[A][B] AND [B][A] return the identity matrix.
 
-## Write a short comment describing this function
+##makeCacheMatrix creates a list containing a function to
+##1. set the value of the matrix
+##2. get the value of the matrix
+##3. set the value of the inverse matrix
+##4. get the value of the inverse matrix
+## basically serves as a directory
 
 makeCacheMatrix <- function(x = matrix()) {
+  inv <- NULL
+  
+  ##sets matrix
+  set <- function(y) {
+    x <<- y                             ##double arrows due env crossing
+    inv <<- NULL
+  }
 
+  ##gets matrix
+  get <- function()x
+  
+  ##sets inverse
+  setinverse <-function(inverse) inv <<- inverse
+    
+  ##gets inverse
+  getinverse <- function() inv
+  
+  list(set=set, get=get, setinverse=setinverse, getinverse=getinverse)
 }
 
-
-## Write a short comment describing this function
+##The next function actually returns the inverse matrix.
+##  First, it checks whether the inverse has already been computed
+##  by looking throuch cached data--if nothing is found, it computes
+##  the inverse and returns it.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        inv <- x$getinverse()
+        if(!is.null(inv)){       ##sees if there is an inv value
+          message("Getting Inverse from Cache")
+          return(inv)
+  }
+  data <- x$get()
+  inv <- solve(data)
+  x$setinverse(inv)
+  inv
 }
+
